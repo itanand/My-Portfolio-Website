84b27c3ea1013a691779603cb96a6c79922ded51
Anand Mohan <anand1505mohan@gmail.com>
2023-03-05 11:06:33 +0530

    aws logo

--- src/Content.js
+++ src/Content.js
@@ -132,7 +132,7 @@ export const content = {
       {
         title: "AWS Cloud",
         para: "I am familliar with AWS Services, Enjoyed to work with the AWS Sevices. I love to work with the AWS Cloud Services. ",
-        logo: seo,
+        logo: aws,
       },
       {
         title: "DevOps Tools",
